9c846a16ee1467ac418bed93067daaf99108bc02
Joseph Freeman <freemanjj1992@gmail.com>
2021-05-06 10:58:21 -0400

    refactored significantly and worked out bugs

--- index.js
+++ index.js
@@ -6,94 +6,25 @@ const html = require('./src/newHtml');
 const objects = require('./src/inquirerObjects');
 const fs = require('fs');
 const inquirer = require('inquirer');
-let managerCard = [];
-let engineerCard = [];
-let internCard = [];
-
-const addManager = (object) => {
-    const {name, employeeId, email, officeNumber} = object;
-    managerCard.push(new Manager(name, employeeId, email, officeNumber));
-};
-
-const addEngineer = (object) => {
-    const {name, employeeId, email, github} = object;
-    engineerCard.push(new Engineer(name, employeeId, email, github));
-};
-
-const addIntern = (object) => {
-    const {name, employeeId, email, school} = object;
-    internCard.push(new Intern(name, employeeId, email, school));
-};
-
-const writtenManagerCards = () => {
-    for (i = 0; i < managerCard.length; i++) {
-       return html.makeManagerCard(managerCard[i]);
-    }
-};
-
-const writtenEngineerCards = () => {
-    for (i = 0; i < engineerCard.length; i++) {
-       return html.makeEngineerCard(engineerCard[i]);
-    }
-};
-
-const writtenInternCards = () => {
-    for (i = 0; i < internCard.length; i++) {
-       return html.makeInternCard(internCard[i]);
-    }
-};
 
 const chooseAction = (object, data) => {
     switch (object) {
         case 'Manager':
-            action = addManager(data);
+            action = html.addManager(data);
             break;
         case 'Engineer':
-            action = addEngineer(data);
+            action = html.addEngineer(data);
             break;
         case 'Intern':
-            action = addIntern(data);
+            action = html.addIntern(data);
             break;
     };
     return action;
 };
 
-const generateBody = () => {
-    return `
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Organization</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="./dist/style.css">
-    </head>
-    <body>
-        <div class="team-boxed">
-            <div class="container">
-                <div class="intro">
-                    <h2 class="text-center">Team </h2>
-                    <p class="text-center">Here is a look at our team!</p>
-                </div>
-                <div class="row people">
-                    ${writtenManagerCards()}
-                    ${writtenEngineerCards()}
-                    ${writtenInternCards()}
-                </div>
-            </div>
-        </div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-    </body>
-    </html>
-    `
-    };
-
 const writeFile = () => {
     fs.writeFile(
-        `team.html`, generateBody(),
+        `team.html`, html.generateBody(),
 (err) => err ? console.error(err) : console.log('Response Recorded!')
         )
 };
--- src/newHtml.js
+++ src/newHtml.js
@@ -1,39 +1,91 @@
-const makeManagerCard = (data) => {
-    const {name, id, email, officeNumber} = data
-    return  `
+const Manager = require('./../lib/manager');
+const Engineer = require('./../lib/engineer');
+const Intern = require('./../lib/intern');
+let managers = [];
+let engineers = [];
+let interns = [];
+let managerCard = [];
+let engineerCard = [];
+let internCard = [];
+
+const generateBody = () => {
+    return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Organization</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="./dist/style.css">
+    </head>
+    <body>
+        <div class="team-boxed">
+            <div class="container">
+                <div class="intro">
+                    <h2 class="text-center">Team </h2>
+                    <p class="text-center">Here is a look at our team!</p>
+                </div>
+                <div class="row people">
+                    ${managerCard.join(' ')}
+                    ${engineerCard.join(' ')}
+                    ${internCard.join(' ')}
+                </div>
+            </div>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    </body>
+    </html>
+    `
+};
+const addManager = (object) => {
+    const {name, employeeId, email, officeNumber} = object;
+    managers.push(new Manager(name, employeeId, email, officeNumber));
+    managerCard.push(
+        `
     <div class="col-md-6 col-lg-4 item">
     <h3 class="name">${name}</h3>
     <p class="title">Manager</p>
-    <p class="description">EmployeeID${id}</p>
+    <p class="description">EmployeeID${employeeId}</p>
     <p class="description">Email:${email}</p>
     <p class="description">Office Number:${officeNumber}</p>
     </div>
     `
+    );
 };
 
-const makeEngineerCard = (data) => {
-    const {name, id, email, github} = data
-    return `
+const addEngineer = (object) => {
+    const {name, employeeId, email, github} = object;
+    engineers.push(new Engineer(name, employeeId, email, github));
+    engineerCard.push(
+        `
     <div class="col-md-6 col-lg-4 item">
     <h3 class="name">${name}</h3>
     <p class="title">Engineer</p>
-    <p class="description">EmployeeID${id}</p>
+    <p class="description">EmployeeID${employeeId}</p>
     <p class="description">Email:${email}</p>
     <p class="description">Github:${github}</p>
     </div>
     `
+    );
 };
 
-const makeInternCard = (data) => {
-    const {name, id, email, school} = data
-    return `
+const addIntern = (object) => {
+    const {name, employeeId, email, school} = object;
+    interns.push(new Intern(name, employeeId, email, school));
+    internCard.push(
+        `
     <div class="col-md-6 col-lg-4 item">
     <h3 class="name">${name}</h3>
     <p class="title">Intern</p>
-    <p class="description">EmployeeID${id}</p>
+    <p class="description">EmployeeID${employeeId}</p>
     <p class="description">Email:${email}</p>
     <p class="description">School:${school}</p>
     </div>
     `
+    );
 };
-module.exports = {makeEngineerCard, makeInternCard, makeManagerCard,};
+    
+module.exports = {addManager, addEngineer, addIntern, generateBody, managerCard, managers, engineerCard, engineers, internCard, interns};
